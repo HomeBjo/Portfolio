bd69c63cb998da46c76f382130a9ee4dc655a965
HomeBjo <122940319+HomeBjo@users.noreply.github.com>
2024-03-01 16:49:22 +0100

    myPortfolio

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
+  // isHovered: boolean = false;
+  imgs = ['win1.jpg', 'win2.jpg'];
 
+  description = [
+    'Task Manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+    'Play a little jump and run by helping Pepe named Ell pollo loco on his adventure',
+  ];
+  title = ['Join','El pollo locco']
+
+  //  onHover() {
+  //   this.isHovered = true;
+  //   console.log('true')
+  // }
+  
+  //  onMouseOut() {
+  //   this.isHovered = false;
+  //   console.log('false')
+  // }
+  
 }
