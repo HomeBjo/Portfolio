86b3beb091ba7f52ad3d20b47062c24b1ab6a28c
HomeBjo <122940319+HomeBjo@users.noreply.github.com>
2024-03-09 12:42:56 +0100

    media Portfolio

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -10,34 +10,18 @@ import AOS from 'aos';
   styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-  // isHovered: boolean = false;
   imgs = ['640x480macmbook-Kanban.png', '640x480 Desktop-Pollo.png'];
 
   description = [
     'Task Manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
     'Play a little jump and run by helping Pepe named Ell pollo loco on his adventure',
   ];
-  title = ['Join','El Pollo Loco'];
-  usedTech = ['JavaScript | Html | CSS','JavaScript | Html | CSS'];
+  title = ['Join', 'El Pollo Loco'];
+  usedTech = ['JavaScript | Html | CSS', 'JavaScript | Html | CSS'];
 
-
- 
-  constructor(){
+  constructor() {
     if (typeof document !== 'undefined') {
-      AOS.init(); 
+      AOS.init();
     }
-      }
-
-
-  
-  //  onHover() {
-  //   this.isHovered = true;
-  //   console.log('true')
-  // }
-  
-  //  onMouseOut() {
-  //   this.isHovered = false;
-  //   console.log('false')
-  // }
-  
+  }
 }
