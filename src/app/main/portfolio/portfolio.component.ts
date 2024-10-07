22df87fcef146e8d4c2aa429c477e00b0b1524a0
HomeBjo <lala2-wuppertal@hotmail.com>
2024-10-07 18:14:34 +0200

    Update

--- src/app/main/portfolio/portfolio.component.ts
+++ src/app/main/portfolio/portfolio.component.ts
@@ -11,27 +11,27 @@ import AOS from 'aos';
   styleUrl: './portfolio.component.scss',
 })
 export class PortfolioComponent {
-  imgs = ['dabubble.png','640x480macmbook-Kanban.png', '640x480 Desktop-Pollo.png'];
+  imgs = ['videoflix_mockup2.jpg','dabubble.png','640x480macmbook-Kanban.png', '640x480 Desktop-Pollo.png'];
 
-  description = [
+  description = ['A video platform for watching videos in different quality levels. Enjoy a user-friendly interface for an optimal video experience, whether you want to discover new content or watch your favorite videos.',
     'Chat-app inspired by Slack. Keep in contact with your colleagues and organize the next steps in your company.',
     'Task Manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
     'Embark on an adventure with Pepe in the exciting game "El Polo Loco." Join Pepe in this captivating jump-and-run experience!',
   ];
 
-  title = ['DABubble','Join', 'El Pollo Loco'];
-  usedTech = ['TypeScript | Html | SCSS','JavaScript | Html | CSS', 'JavaScript | Html | CSS'];
-  links = [
+  title = ['Videoflix','DABubble','Join', 'El Pollo Loco'];
+  usedTech = ['Django REST | Python | Angular | TypeScript','TypeScript | Html | SCSS','JavaScript | Html | CSS', 'JavaScript | Html | CSS'];
+  links = ['https://videoflix.xn--bjrnteneicken-jmb.de/',
     'https://dabubble.xn--bjrnteneicken-jmb.de/index.html',
     'https://xn--bjrnteneicken-jmb.de/join/html/login.html',
     'https://xn--bjrnteneicken-jmb.de/el_pollo_loco/index.html',
   ];
-  gitLinks = [
+  gitLinks = ['https://github.com/HomeBjo/Videoflix',
     'https://github.com/HomeBjo/DABubble.git',
     'https://github.com/HomeBjo/JOIN',
     'https://github.com/HomeBjo/el_pollo_loco',
   ];
-  projectKeys = ['bubble','join', 'elpollo'];
+  projectKeys = ['videoflix','bubble','join', 'elpollo'];
 
   constructor() {
     if (typeof document !== 'undefined') {
